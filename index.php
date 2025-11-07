d1727171d970bcb9208037131d5cb6f80f18b264
Ajsneed200 <ajsneed200@mail.cfcc.edu>
2025-11-07 11:40:30 -0500

    Enhance index.php with title and author details
    Added a title and author information to the HTML.

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
-<html>
+  <html>
   <head>
     
   </head>
   <body>
-
+<h1>Building a Dynamic Form with PHP</h1>
+<p>Andrew Sneed</p>
+<p>11/7/2025</p>
 
   </body>
-</html>
+  </html>
