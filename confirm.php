b7dded9d76c8890f4ef54d0ad98177e74f40a408
Ajsneed200 <ajsneed200@mail.cfcc.edu>
2025-11-10 21:34:11 -0500

    Implement form validation and sanitization
    Added form validation and sanitization for user inputs.

--- confirm.php
+++ confirm.php
@@ -1,1 +1,48 @@
+<!DOCTYPE HTML>
+    <html>
 
+    <head>
+<link rel="stylesheet" href="main.css">
+    </head>
+    <body>
+<h1>Building a Dynamic Form with PHP</h1>
+<p>Andrew Sneed</p>
+<p>11/7/2025</p>
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST");
+{
+    /*Validation
+    Makess inputs follow the set rules.*/
+    $name = trim($_POST["name"]);
+    $email = trim($_POST["email"]);
+    $message = trim($_POST["message"]);
+    $sanitize_email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
+    /*Sanitization
+    Removes any potentially harmful data*/
+    
+    if (empty($name)) {
+        $error;
+    }
+
+    if (empty($sanitize_email)) {
+        $error;
+    }
+
+    if (empty($message)) {
+        $error;
+    }
+
+    if (!empty($error)) {
+        $class = "error";
+        echo "<h4>Error: All fields must be filled out.</h4>";
+    } else {
+        $class = "success";
+        echo "<h4>All fields filled out successfully.</h4>";
+    }
+
+}
+echo "<h2>Name: ".$name."<br/> Email: ".$sanitize_email."<br/> Message: ".$message."</h2>";
+?>
+    </body>
+    
+    </html>
