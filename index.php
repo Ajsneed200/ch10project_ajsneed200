a7b504ca1121d8bca5f935ba8659a05aeacb9a1c
Ajsneed200 <ajsneed200@mail.cfcc.edu>
2025-11-20 13:31:07 -0500

    Add media attribute to stylesheet link

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     <html>
 
     <head>
-        <link rel="stylesheet" type="text/css" href="styles/main.css" />
+        <link rel="stylesheet" type="text/css" href="styles/main.css" media = "screen, print" />
     </head>
     <body>
 <h1>Building a Dynamic Form with PHP</h1>
